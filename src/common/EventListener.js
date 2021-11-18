f27a8ff497f93eb90bb7a16a1193a0fe7b1bd9c8
Seph <user@local.host>
2021-11-18 17:56:23 +1000

    This may need to chang eto be more accurate

--- src/common/EventListener.js
+++ src/common/EventListener.js
@@ -8,9 +8,9 @@ export class Listener {
   static connect() {
     Listener.src = new EventSource(Listener.url + "?token=" + window.localStorage.getItem("token"))
     window.debugListener = Listener;
-    Listener.src.onopen = () => { window.$("#nointernet").nag("hide") }
+    //Listener.src.onopen = () => { window.$("#nointernet").nag("hide") }
     Listener.src.onerror = () => { window.$("#nointernet").nag("show") };
-    Listener.src.onmessage = () => { window.$("#nointernet").nag("show") };
+    Listener.src.onmessage = () => { window.$("#nointernet").nag("hide") };
   }
 
 }
